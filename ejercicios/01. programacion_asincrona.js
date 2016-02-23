253f28d0e5daadfb0ac705b946e136c18a333072
José Manuel Ciges Regueiro <jmanuel@ciges.net>
2016-02-23 06:04:53 -0500

    Suprimidael antiguo ejercicio de websockets. Cambiado el nombre de los archivos y carpetas por inválidos

--- ejercicios/01. programacion_asincrona.js
+++
@@ -1,6 +0,0 @@
-process.stdin.on('data', function(data) {
-    process.stdout.write("\nHas escrito " + data.toString().trim() + "\n");
-	process.exit();
-});
-
-process.stdout.write ("¿Cuál es tu hobby favorito?: ");
--- ejercicios/09. websockets/ws.js
+++
@@ -1,8 +0,0 @@
-var WebSocketServer = require("ws").Server;
-var wss = new WebSocketServer({ port: 9000 });
-
-wss.on("connection", function(ws) {
-
-	ws.send("Bienvenido al servidor de chat");
-
-});
